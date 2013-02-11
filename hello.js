da6e4ae26c1b5262f1afe5bf051f13d05b9e6ffd
André Morales <labmorales@gmail.com>
2013-02-11 00:31:35 -0200

    Modificacoes no hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,22 @@
-var http = require("http");
+var http = require("http"), fs = require("fs");
 
 http.createServer(function(request, response){
-	response.writeHead(200);
-	response.write("Hello World\n");
-	setTimeout(function(){
-		response.write("Goobye World");
+	var new_file = fs.createWriteStream("log.txt");
+	var total = request.headers["content-length"];
+	var uploadBytes = 0;
+
+	request.pipe(new_file);
+
+	request.on("data", function(chunk){
+		uploadBytes += chunk.length;
+		var porcentagem = ( uploadBytes / total) * 100;
+		console.log("Progresso:" + porcentagem);		
+	});
+
+	request.on("end", function(){
 		response.end();
-	}, 5000);
-	
+	});
+
 }).listen(8080);
 
 console.log("Escutando na 8080");
